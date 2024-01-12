f1d1a913f872575b15331621b75e20d087967a0e
oxstzwy <140463222+oxstzwy@users.noreply.github.com>
2024-01-12 15:52:04 -0400

    provedor map

---
+++ node_652f5ac70c34b.tmp.js
@@ -0,0 +1,38 @@
+var map = L.map('map').setView([-3.096, -59.969], 14.5);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+    L.marker([-3.096, -59.969]).addTo(map)
+        .bindPopup('welcome to Hell')
+        .openPopup(); 
+
+
+/* codigo teste para medir distancias de feiçoes
+var osmap = L.tileLayer.province('OpenStreetMap.Mapnik',);
+var Stadia_StamenWatercolor = L.tileLayer.province('Stamen.Watercolor');
+
+var BaseMaps = {
+    osm: osmap,
+    'Stamen Watercolor': Stadia_StamenWatercolor
+}
+
+var map = L.map('map', {
+    center:[-3.096, -59.969],
+    layers:[osmap]
+});
+
+var mapLayers = L.control.layers(BaseMaps).addTo(map);
+
+
+
+var ctlMeasure = L.control.polylineMeasure({
+	position: "topleft",
+	measureControlTitle: "Measure Lenght",
+}).addTo(map);
+*/
+
+// Adicionar camandas vetoriais Base de dados 
+
+npm install leaflet-providers
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([-3.096, -59.969], 14.5);
+const map = L.map('map').setView([-3.096, -59.969], 14.5);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -9,24 +9,47 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         .openPopup(); 
 
 
-/*var osmap = L.tileLayer.province('OpenStreetMap.Mapnik',);
-var Stadia_StamenWatercolor = L.tileLayer.province('Stamen.Watercolor');
+// codigo teste para diferentes tipos de mapa base (não funcionando)
 
-var BaseMaps = {
-    osm: osmap,
-    'Stamen Watercolor': Stadia_StamenWatercolor
+/*
+var osmMap = L.tileLayer.provider('OpenStreetMap.Mapnik');
+var StamenMap = L.tileLayer.provider('Stadia.StamenWatercolor');
+var imageryMap = L.tileLayer.provider('Esri.WorldImagery');
+
+var baseMaps = {
+    OSM: osmMap,
+    'Stamen Watercolor': StamenMap,
+    'Word Imagery': imageryMap
 }
 
 var map = L.map('map', {
-    center:[-3.096, -59.969],
-    layers:[osmap]
+    center: [-3.096, -59.969],
+    zoom: 14,
+    layers: [osmMap]
+
 });
 
-var mapLayers = L.control.layers(BaseMaps).addTo(map);
+var mapLayers = L.control.layers(baseMaps).addTo(map);
+/*
 
-*/
-
+/* ferramenta de dimensão comprimento
 var ctlMeasure = L.control.polylineMeasure({
 	position: "topleft",
 	measureControlTitle: "Measure Lenght",
 }).addTo(map);
+*/
+
+// Adicionar camandas WMS Base de dados geoserver
+
+let geoServerIPPort = 'localhost8008';
+let geoServerWorkspace = 'campus-map';
+let stateLayerName = 'campus-map:ind_st';
+
+let vetores = 
+
+// adicionar coordenadas no cursor 
+
+map.on("click", e => {
+    const {lat, lng} = e.latlng
+    console.log (lat, ln) //ver algum metodo de mostrar na tela exp: alert
+    })
